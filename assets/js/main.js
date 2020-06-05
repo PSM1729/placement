5a0e78edb35bcc9c6b5b208f12a54c6558971060
vanshikapandey <pvanshik217@gmail.com>
2020-06-05 16:10:42 +0530

    js file for IIIT Guwahati Placement

--- assets/js/main.js
+++ assets/js/main.js
@@ -135,3 +135,456 @@
   });
 
 })(jQuery);
+//JS file for IIIT  Guwahati placement
+
+
+let myChart1 = document.getElementById('myChart1').getContext('2d');
+
+        Chart.defaults.global.defaultFontFamily= 'Lato';
+        Chart.defaults.global.defaultFontSize= 18;
+        Chart.defaults.global.defaultFontColor ='#777';
+        
+
+
+        let secondBatch = new Chart(myChart1, {
+            type:'bar',
+            data:{
+                 
+                
+                labels:['ECE','CSE','Overall'],
+                
+                datasets:[{
+                    label:'Total Students registered',
+                    data:[
+
+                        27,
+                        32,
+                        59
+                    ],
+                    //backgroundColor:'green',
+                    backgroundColor:"green",
+                    boderWidth:4,
+                    borderColor:"#777",
+                    hoverBorderWidth:2,
+                    hoverBorderColor:"#000"
+
+                },
+                {
+                    label:'Total job offered',
+                    data:[
+
+                        11,
+                        49,
+                        60
+                    ],
+                    backgroundColor:'orange',
+                    boderWidth:4,
+                    borderColor:"#777",
+                    hoverBorderWidth:2,
+                    hoverBorderColor:"#000"
+
+                },
+                {
+                label:'Total Students selected',
+                    data:[
+
+                        10,
+                        25,
+                        35
+                    ],
+                    //backgroundColor:'green',
+                    backgroundColor:"blue",
+                    boderWidth:4,
+                    borderColor:"#777",
+                    hoverBorderWidth:2,
+                    hoverBorderColor:"#000"
+                },
+                {
+                    label:'Students going for higher studies',
+                    data:[
+
+                        16,
+                        4,
+                        20
+                    ],
+                    //backgroundColor:'green',
+                    backgroundColor:"Yellow",
+                    boderWidth:4,
+                    borderColor:"#777",
+                    hoverBorderWidth:2,
+                    hoverBorderColor:"#000"
+
+                },
+
+                {
+                    label:'Students starting their own company',
+                    data:[
+
+                       1,
+                       0,
+                       1
+                    ],
+                    //backgroundColor:'green',
+                    backgroundColor:"black",
+                    boderWidth:4,
+                    borderColor:"#777",
+                    hoverBorderWidth:2,
+                    hoverBorderColor:"#000"
+
+
+                },
+                {
+                    label:'Average salary in LPA',
+                    data:[
+
+                       5.19,
+                       12.75,
+                       10.1
+                    ],
+                    //backgroundColor:'green',
+                    backgroundColor:"purple",
+                    boderWidth:4,
+                    borderColor:"#777",
+                    hoverBorderWidth:2,
+                    hoverBorderColor:"#000"
+                },
+                {
+                    
+                    label:'Highest Salary in LPA',
+                    data:[
+
+                       7,
+                       56,
+                       56
+                    ],
+                    //backgroundColor:'green',
+                    backgroundColor:"red",
+                    boderWidth:4,
+                    borderColor:"#777",
+                    hoverBorderWidth:2,
+                    hoverBorderColor:"#000"
+                
+                }
+                
+                ],
+            },
+            options:{
+                title:{
+                display:true,
+                text:"Placement Status for the year 2018-19",
+                fontSize:25,
+                fontColor:'#000'
+            },
+            legend:{
+                position:"right",
+                labels:{
+                    fontColor:'#000'
+                },
+                layout:{
+                    padding:{
+                        left:50,
+                        right:0,
+                        bottom:0,
+                        top:0
+                    }
+                }
+
+            }
+            }
+        });
+
+        let myChart2 = document.getElementById('myChart2').getContext('2d');
+
+        Chart.defaults.global.defaultFontFamily= 'Lato';
+        Chart.defaults.global.defaultFontSize= 18;
+        Chart.defaults.global.defaultFontColor ='#777';
+        
+
+
+        let pie1 = new Chart(myChart2, {
+            type:'pie',
+            data:{
+                 
+                
+                labels:['ECE','CSE','Overall'],
+                
+                datasets:[{
+                    label:'Percentage of students placed',
+                    data:[
+
+                        37,
+                        83,
+                        60
+                    ],
+                    //backgroundColor:'green',
+                    backgroundColor:
+                    
+                    [
+                    'rgba(0, 191, 255, 0.8)',
+                    'rgba(0, 0, 255, 0.8)',
+                    'rgba(0, 255, 255, 0.8)'
+                    ],
+                    
+                    boderWidth:4,
+                    borderColor:"#777",
+                    hoverBorderWidth:2,
+                    hoverBorderColor:"#000"
+
+                }
+
+            ],
+        },
+        options:{
+            title:{
+            display:true,
+            text:"Percentage of students placed in the year 2018-19",
+            fontSize:25,
+            fontColor:'#000'
+        },
+        legend:{
+            position:"right",
+            labels:{
+                fontColor:'#000'
+            },
+            layout:{
+                padding:{
+                    left:50,
+                    right:0,
+                    bottom:0,
+                    top:0
+                }
+            }
+
+        }
+        }
+    });
+        
+        
+        
+        let myChart3 = document.getElementById('myChart3').getContext('2d');
+
+        Chart.defaults.global.defaultFontFamily= 'Lato';
+        Chart.defaults.global.defaultFontSize= 18;
+        Chart.defaults.global.defaultFontColor ='#777';
+        
+
+
+        let firstBatch = new Chart(myChart3, {
+            type:'bar',
+            data:{
+                 
+                
+                labels:['ECE','CSE','Overall'],
+                
+                datasets:[{
+                    label:'Total Students registered',
+                    data:[
+
+                        26,
+                        32,
+                        58
+                    ],
+                    //backgroundColor:'green',
+                    backgroundColor:"green",
+                    boderWidth:4,
+                    borderColor:"#777",
+                    hoverBorderWidth:2,
+                    hoverBorderColor:"#000"
+
+                },
+                {
+                    label:'Total job offered',
+                    data:[
+
+                        13,
+                        30,
+                        43
+                    ],
+                    backgroundColor:'orange',
+                    boderWidth:4,
+                    borderColor:"#777",
+                    hoverBorderWidth:2,
+                    hoverBorderColor:"#000"
+
+                },
+                {
+                label:'Total Students selected',
+                    data:[
+
+                        11,
+                        22,
+                        33
+                    ],
+                    //backgroundColor:'green',
+                    backgroundColor:"blue",
+                    boderWidth:4,
+                    borderColor:"#777",
+                    hoverBorderWidth:2,
+                    hoverBorderColor:"#000"
+                },
+                {
+                    label:'Students going for higher studies',
+                    data:[
+
+                        5,
+                        2,
+                        7
+                    ],
+                    //backgroundColor:'green',
+                    backgroundColor:"Yellow",
+                    boderWidth:4,
+                    borderColor:"#777",
+                    hoverBorderWidth:2,
+                    hoverBorderColor:"#000"
+
+                },
+
+                {
+                    label:'Students starting their own company',
+                    data:[
+
+                       1,
+                       1,
+                       2
+                    ],
+                    //backgroundColor:'green',
+                    backgroundColor:"black",
+                    boderWidth:4,
+                    borderColor:"#777",
+                    hoverBorderWidth:2,
+                    hoverBorderColor:"#000"
+
+
+                },
+                
+                {
+                    label:'Average salary in LPA',
+                    data:[
+
+                       6.56,
+                       6.7,
+                       6.68
+                    ],
+                    //backgroundColor:'green',
+                    backgroundColor:"purple",
+                    boderWidth:4,
+                    borderColor:"#777",
+                    hoverBorderWidth:2,
+                    hoverBorderColor:"#000"
+                },
+                {
+                    
+                    label:'Highest Salary in LPA',
+                    data:[
+
+                       15,
+                       17,
+                       17
+                    ],
+                    //backgroundColor:'green',
+                    backgroundColor:"red",
+                    boderWidth:4,
+                    borderColor:"#777",
+                    hoverBorderWidth:2,
+                    hoverBorderColor:"#000"
+                
+                }
+                
+                ],
+            },
+            options:{
+                title:{
+                display:true,
+                text:"Placement Status for the year 2017-18",
+                fontSize:25,
+                fontColor:'#000'
+            },
+            legend:{
+                position:"right",
+                labels:{
+                    fontColor:'#000'
+                },
+                layout:{
+                    padding:{
+                        left:50,
+                        right:0,
+                        bottom:0,
+                        top:0
+                    }
+                }
+
+            }
+            }
+        });
+
+
+
+        let myChart4 = document.getElementById('myChart4').getContext('2d');
+
+        Chart.defaults.global.defaultFontFamily= 'Lato';
+        Chart.defaults.global.defaultFontSize= 18;
+        Chart.defaults.global.defaultFontColor ='#777';
+        
+
+
+
+        let pie2 = new Chart(myChart4, {
+            type:'pie',
+            data:{
+                 
+                
+                labels:['ECE','CSE','Overall'],
+                
+                datasets:[{
+                    label:'Percentage of students placed',
+                    data:[
+
+                        65.4,
+                        78.1,
+                        72.4
+                    ],
+                    //backgroundColor:'green',
+                    backgroundColor:
+                    
+                    [
+                    'rgba(0, 191, 255, 0.8)',
+                    'rgba(0, 0, 255, 0.8)',
+                    'rgba(0, 255, 255, 0.8)'
+                    ],
+                    
+                    boderWidth:4,
+                    borderColor:"#777",
+                    hoverBorderWidth:2,
+                    hoverBorderColor:"#000"
+
+                }
+
+            ],
+        },
+        options:{
+            title:{
+            display:true,
+            text:"Percentage of students placed in the year 2017-18",
+            fontSize:25,
+            fontColor:'#000'
+        },
+        legend:{
+            position:"right",
+            labels:{
+                fontColor:'#000'
+            },
+            layout:{
+                padding:{
+                    left:50,
+                    right:0,
+                    bottom:0,
+                    top:0
+                }
+            }
+
+        }
+        }
+    });
+        
+        
+        
