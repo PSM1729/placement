cf4191c11f822a802f5180de757b2c933189c7ed
riyarana10 <66198709+riyarana10@users.noreply.github.com>
2020-06-16 18:38:35 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -269,6 +269,8 @@ let myChart1 = document.getElementById('myChart1').getContext('2d');
                 ],
             },
             options:{
+              responsive:true,
+              maintainAspectRatio:false,
                 title:{
                 display:true,
                 text:"Placement Status for the year 2018-19",
@@ -335,6 +337,8 @@ let myChart1 = document.getElementById('myChart1').getContext('2d');
             ],
         },
         options:{
+          responsive:true,
+          maintainAspectRatio:false,
             title:{
             display:true,
             text:"Percentage of students placed in the year 2018-19",
@@ -493,6 +497,8 @@ let myChart1 = document.getElementById('myChart1').getContext('2d');
                 ],
             },
             options:{
+              responsive:true,
+              maintainAspectRatio:false,
                 title:{
                 display:true,
                 text:"Placement Status for the year 2017-18",
@@ -562,6 +568,8 @@ let myChart1 = document.getElementById('myChart1').getContext('2d');
             ],
         },
         options:{
+          responsive:true,
+          maintainAspectRatio:false,
             title:{
             display:true,
             text:"Percentage of students placed in the year 2017-18",
